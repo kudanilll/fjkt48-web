2ee174e6b6ef2b46c62a96f02ab8a2cccbb9a7a6
achmaddaniel24 <achmad24daniel@gmail.com>
2024-01-15 13:18:40 +0700

    styles(pagination): change pagination styles.

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,5 +1,5 @@
-import leftIcon  from "@/assets/icons/chevron-left.svg";
-import rightIcon from "@/assets/icons/chevron-right.svg";
+import leftIcon  from "@/assets/icons/arrow-left.svg";
+import rightIcon from "@/assets/icons/arrow-right.svg";
 import Image from "next/image";
 
 type PaginationProps = {
@@ -21,13 +21,11 @@ export default function Pagination(props: PaginationProps) {
   }
   
   return (
-    <div className="flex items-center justify-center py-10 lg:px-0 sm:px-6 px-4">
+    <div className="flex items-center justify-center py-4 lg:px-0 sm:px-6 px-4">
       <div className="lg:w-3/5 w-full flex items-center justify-between">
         <div className="flex items-center pt-3 text-gray-800 cursor-pointer">
-          <button onClick={prev}>
-            <Image src={leftIcon} alt="prev" width={24} height={24}/>
-          </button>
-          <p className="text-sm ml-1 font-medium leading-none">Sebelumnya</p>
+          <Image onClick={prev} src={leftIcon} alt="prev" width={24} height={24}/>
+          <p onClick={prev} className="text-sm ml-1 font-medium leading-none">Sebelumnya</p>
         </div>
         <div className="sm:flex hidden">
           <p className="text-sm font-light pt-3 px-1">Halaman </p>
@@ -36,10 +34,8 @@ export default function Pagination(props: PaginationProps) {
           <p className="text-sm font-medium pt-3 px-1">{props.total}</p>
         </div>
         <div className="flex items-center pt-3 text-gray-800 cursor-pointer">
-          <p className="text-sm font-medium leading-none mr-1">Selanjutnya</p>
-          <button onClick={next}>
-            <Image src={rightIcon} alt="next" width={24} height={24}/>
-          </button>
+          <p onClick={next} className="text-sm font-medium text-center whitespace-nowrap mr-1">Selanjutnya</p>
+          <Image onClick={next} src={rightIcon} alt="next" width={24} height={24}/>
         </div>
       </div>
     </div>
