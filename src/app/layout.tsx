7fb57b6e37e16e51a6e794825dc954ad3c5daab5
achmaddaniel24 <achmad24daniel@gmail.com>
2024-01-21 20:55:27 +0700

    feat: add top loader.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import NavigationBar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "@/assets/styles/globals.css";
@@ -15,6 +16,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="scroll-smooth" style={{scrollBehavior:"smooth"}}>
       <body className={sora.className}>
+        <NextTopLoader
+          color="#E53935"
+          showSpinner={false}/>
         <main className="flex flex-col min-h-screen bg-slate-100 text-black">
           <NavigationBar/>
           {children}
