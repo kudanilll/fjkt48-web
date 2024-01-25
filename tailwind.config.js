a74aab376c73074663d297aedfeac6e2185fdf1d
achmaddaniel24 <achmad24daniel@gmail.com>
2024-01-25 18:23:06 +0000

    ci: change tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss/types').Config} */
 const config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
