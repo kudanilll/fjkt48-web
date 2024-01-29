5ba8e643d7dbbc4747fb15142ecdbf238bded5ba
achmaddaniel24 <achmad24daniel@gmail.com>
2024-01-29 18:23:39 +0700

    perf: delete unused code.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { navigation } from "./navigation.json";
 import { usePathname } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Cross as Hamburger } from "hamburger-react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function NavigationBar() {
   const pathname = usePathname() || "/";
