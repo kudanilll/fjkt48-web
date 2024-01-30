6bbc9c12b87306bba7397852ae51e256ecc5ed7c
achmaddaniel24 <achmad24daniel@gmail.com>
2024-01-30 13:16:38 +0700

    build: update depedencies.

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/fjkt48-5bc17.appspot.com/o/**",
+      },
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
