9217ff78bb58d1c64e39a40866ac94fd307031fc
kudanill. <achmad24daniel@gmail.com>
2024-02-11 04:28:24 +0700

    styles(shop): responsive carousel.

--- src/components/card/ProductCard/index.tsx
+++ src/components/card/ProductCard/index.tsx
@@ -5,29 +5,25 @@ type ProductCardProps = {
   image: string;
   title: string;
   price: number;
-  realPrice: number;
-  status: string;
+  status: "available" | "pre-order" | "not-available";
   slug: string;
 };
 
 export default function ProductCard(props: ProductCardProps) {
   return (
     <div>
-      <div className="rounded-lg flex flex-col overflow-hidden bg-gray-300">
+      <div className="relative w-full max-w-xs rounded-lg flex flex-col overflow-hidden bg-gray-300">
         <Image
-          className="object-cover rounded-t-lg"
-          src="/product-image.jpg"
-          alt="product image"
+          className="object-fill rounded-t-lg"
+          src={props.image}
+          alt={props.title}
           width={200}
           height={200}
         />
         <div className="p-2">
-          <h5 className="text-md font-regular text-zinc-900">Sepatu</h5>
-          <p>
-            <span className="text-2xl font-semibold text-zinc-900">Rp5000</span>
-            <span className="text-sm text-zinc-700 line-through">Rp10000</span>
-          </p>
-          <button className="mt-2 flex items-center justify-center rounded-md bg-zinc-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-zinc-700 focus:outline-none">
+          <h5 className="text-md font-regular text-zinc-900">{props.title}</h5>
+          <span className="text-2xl font-semibold text-zinc-900">{`Rp${props.price}`}</span>
+          <button className="mt-2 flex mx-auto items-center justify-center rounded-md bg-zinc-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-zinc-700 focus:outline-none">
             Cek Selengkapnya
           </button>
         </div>
--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -2,35 +2,56 @@
 import ProductCard from "@/components/card/ProductCard";
 // Import Swiper React components & styles
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Mousewheel } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/free-mode";
 
 type CarouselProps = {};
 
 export default function Carousel(props: CarouselProps) {
   return (
     <Swiper
-      slidesPerView={2}
+      // slidesPerView={5}
+      // spaceBetween={10}
       spaceBetween={10}
+      slidesPerView="auto"
+      breakpoints={{
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        800: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 10,
+        },
+      }}
       freeMode={true}
+      mousewheel={true}
+      modules={[FreeMode, Mousewheel]}
       className="mySwiper">
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
+      {[...Array(10)].map((_, index) => (
+        <SwiperSlide key={index}>
+          <ProductCard
+            image="/product-image.jpg"
+            title="Sepatu"
+            price={5000}
+            status={"available"}
+            slug={""}
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
