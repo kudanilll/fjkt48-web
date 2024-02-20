744d2ff82dba41145df4f4c65af6f1d32564a0ca
kudanill. <achmad24daniel@gmail.com>
2024-02-20 16:53:47 +0700

    update metadata not-found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "FJKT48 | Not Found",
+  description: "Halaman Tidak Ditemukan",
+};
 
 export default function NotFound() {
   return (
