613ed6b8b4a542933f5ba6f1f22b2ab9f402cc27
kudanill. <achmad24daniel@gmail.com>
2024-05-05 03:11:42 +0700

    fix: error when render MemberCard.

--- src/common/typedata/member.ts
+++ src/common/typedata/member.ts
@@ -1,20 +1,20 @@
 export type member = {
   id: string;
   name: string;
-  nickname: string;
-  birthday: string;
-  height: string;
-  horoscope: string;
-  blood_group: string;
-  gen: string;
-  idn: string;
+  nickname?: string;
+  birthday?: string;
+  height?: string;
+  horoscope?: string;
+  blood_group?: string;
+  gen?: string;
+  idn?: string;
   image: string;
-  instagram: string;
-  jikoshoukai: string;
-  showroom: string;
-  x: string;
-  tiktok: string;
-  active: boolean;
+  instagram?: string;
+  jikoshoukai?: string;
+  showroom?: string;
+  x?: string;
+  tiktok?: string;
+  active?: boolean;
 };
 
 export default member;
