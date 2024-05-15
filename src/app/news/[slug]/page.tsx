332a2ec8b9cdda2a395100c9f032e468a1e5a028
kudanill. <achmad24daniel@gmail.com>
2024-05-15 09:44:41 +0700

    styles: update NotFound UI.

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { getNewsFromStorage } from "@/utils/get-data";
+import NormalPageWrapper from "@/components/wrapper/NormalPageWrapper";
 import md from "markdown-it";
 import matter from "gray-matter";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getNews(slug: string) {
   try {
@@ -15,21 +17,31 @@ async function getNews(slug: string) {
 
 function NotFound() {
   return (
-    <div className="text-center flex flex-col justify-center items-center content-center space-y-4">
-      <Image
-        alt="not found :("
-        src="/assets/not-found.png"
-        width={200}
-        height={200}
-        className="mt-20 mb-4"
-      />
-      <h1 className="font-semibold text-center mb-4">
-        404: Berita tidak ditemukan :(
-      </h1>
-      <article className="font-regular text-center">
-        Buka beranda untuk menemukan berita terbaru!
-      </article>
-    </div>
+    <NormalPageWrapper>
+      <div className="text-center flex flex-col justify-center items-center content-center space-y-4">
+        <Image
+          alt="not found :("
+          src="/assets/not-found.png"
+          width={200}
+          height={200}
+        />
+        <h1 className="hidden md:block text-2xl font-semibold">
+          404 - Berita Tidak Ditemukan :(
+        </h1>
+        <div className="md:hidden">
+          <h1 className="text-3xl font-semibold">404</h1>
+          <h1 className="text-2xl font-semibold">Berita Tidak Ditemukan :(</h1>
+        </div>
+        <p>Buka beranda untuk menemukan berita terbaru!</p>
+        <div className="px-4 py-2 cursor-pointer bg-red-400 md:bg-transparent md:hover:bg-red-400 rounded-full md:hover:text-white duration-300">
+          <Link
+            className="text-white md:text-gray-800 md:hover:text-white duration-300"
+            href="/">
+            Kembali ke beranda
+          </Link>
+        </div>
+      </div>
+    </NormalPageWrapper>
   );
 }
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import NormalPageWrapper from "@/components/wrapper/NormalPageWrapper";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,23 +10,31 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div className="text-center flex flex-col justify-center items-center content-center space-y-4">
-      <Image
-        alt="not found :("
-        src="/assets/not-found.png"
-        width={200}
-        height={200}
-        className="mt-20"
-      />
-      <h1 className="text-2xl font-semibold">
-        404 - Halaman Tidak Ditemukan :(
-      </h1>
-      <p>Halaman yang anda cari tidak ada</p>
-      <Link
-        className="underline text-blue-600 hover:text-red-500 duration-300"
-        href="/">
-        Kembali ke beranda
-      </Link>
-    </div>
+    <NormalPageWrapper>
+      <div className="text-center flex flex-col justify-center items-center content-center space-y-4">
+        <Image
+          alt="not found :("
+          src="/assets/not-found.png"
+          width={200}
+          height={200}
+          className="mt-20"
+        />
+        <h1 className="hidden md:block text-2xl font-semibold">
+          404 - Halaman Tidak Ditemukan :(
+        </h1>
+        <div className="md:hidden">
+          <h1 className="text-3xl font-semibold">404</h1>
+          <h1 className="text-2xl font-semibold">Halaman Tidak Ditemukan :(</h1>
+        </div>
+        <p>Halaman yang anda cari tidak ada</p>
+        <div className="px-4 py-2 cursor-pointer bg-red-400 md:bg-transparent md:hover:bg-red-400 rounded-full md:hover:text-white duration-300">
+          <Link
+            className="text-white md:text-gray-800 md:hover:text-white duration-300"
+            href="/">
+            Kembali ke beranda
+          </Link>
+        </div>
+      </div>
+    </NormalPageWrapper>
   );
 }
