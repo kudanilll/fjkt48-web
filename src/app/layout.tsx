8955016f5f8944a3fa2d4403698fe9b3fd7cd4d4
kudanill. <achmad24daniel@gmail.com>
2024-05-25 23:18:37 +0700

    create SeoMetadata with separate files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SeoMetadata } from "@/seo/SeoMetadata";
 import { Noto_Sans } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import NavigationBar from "@/components/navbar";
@@ -8,27 +9,7 @@ import "@/styles/globals.css";
 
 const noto = Noto_Sans({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("http://localhost:3000"),
-  title: "FJKT48",
-  description: "JKT48 Fans Web",
-  openGraph: {
-    title: "FJKT48",
-    description: "JKT48 Fans Web",
-    url: "https://fjkt48.vercel.app",
-    siteName: "fjkt48.vercel.app",
-    locale: "id_ID",
-    type: "website",
-  },
-  authors: [
-    {
-      name: "Achmad Daniel Syahputra",
-      url: "https://github.com/achmaddaniel24",
-    },
-    { name: "Muhammad Ikhsan Fauzi", url: "https://dribbble.com/ipauscream" },
-  ],
-  keywords: ["jkt48", "idol", "jkt"],
-};
+export const metadata: Metadata = SeoMetadata;
 
 export default function RootLayout({
   children,
---
+++ src/seo/SeoMetadata.ts
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+export const SeoMetadata: Metadata = {
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_DOMAIN}`),
+  title: "FJKT48",
+  description: "JKT48 Fans Web",
+  openGraph: {
+    title: "FJKT48",
+    description: "JKT48 Fans Web",
+    url: process.env.NEXT_PUBLIC_DOMAIN,
+    siteName: "FJKT48",
+    locale: "id_ID",
+    type: "website",
+  },
+  authors: [
+    {
+      name: "Achmad Daniel Syahputra",
+      url: "https://github.com/achmaddaniel24",
+    },
+    { name: "Muhammad Ikhsan Fauzi", url: "https://dribbble.com/ipauscream" },
+  ],
+  keywords: ["jkt48", "idol", "jkt"],
+};
