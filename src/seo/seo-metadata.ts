d2ed56a79a81ff5cf7050d00586349a5f5aad5fc
kudanill. <achmad24daniel@gmail.com>
2024-07-09 14:23:03 +0700

    feat(seo): add OpenGraph image for metadata.

--- src/seo/seo-metadata.ts
+++ src/seo/seo-metadata.ts
@@ -8,12 +8,24 @@ export const SeoMetadata: Metadata = {
   icons: { icon: "/favicon.ico" },
   description: "JKT48 Fans Web",
   openGraph: {
-    title: "FJKT48",
+    title: "FJKT48 - Home",
     description: "JKT48 Fans Web",
     url: process.env.NEXT_PUBLIC_DOMAIN,
     siteName: "FJKT48",
     locale: "id_ID",
     type: "website",
+    images: [
+      {
+        url: process.env.NEXT_PUBLIC_DOMAIN + "/opengraph-image-512.jpg",
+        width: 512,
+        height: 512,
+      },
+      {
+        url: process.env.NEXT_PUBLIC_DOMAIN + "/opengraph-image-1920-1080.jpg",
+        width: 3264,
+        height: 1836,
+      },
+    ],
   },
   authors: [
     {
